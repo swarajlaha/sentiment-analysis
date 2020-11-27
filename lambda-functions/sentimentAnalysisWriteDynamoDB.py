722099f5b213cf0cecca297e85e766308e31a21c
swarajlaha <swaraj.srj@gmail.com>
2020-11-27 16:34:20 +0530

    lambda func to write to DynamoDB

---
+++ lambda-functions/sentimentAnalysisWriteDynamoDB.py
@@ -0,0 +1,37 @@
+#Lambda function to PUT review data to DynamoDB, for all triggers, irrespective of the sentiment.
+
+import boto3
+
+def lambda_handler(event, context):
+    '''
+    Adds the review data to the review database table
+    '''
+    
+    #select correct table based on input data
+    dynamodb = boto3.client('dynamodb')
+    if event['reviewType'] == 'Product':
+        tableName = 'sentiment-analysis-products'
+    else:
+        raise Exception("Input review is neither Product nor Service")
+    
+    #construct response to put data in table
+    response = dynamodb.put_item(
+        TableName=tableName,
+        Item={
+            'reviewID': {"N": event['reviewID'] },
+            'customerID': {"N": event['customerID'] },
+            'productID': {"N": event['productID'] },
+            'feedback': {"S": event['feedback'] },
+            'sentiment': {"S": event['sentiment'] }
+        },
+    )
+    
+    #pass through values 
+    response['reviewType'] = event['reviewType']
+    response['reviewID'] = event['reviewID']
+    response['customerID'] = event['customerID']
+    response['productID'] = event['productID']
+    response['feedback'] = event['feedback']
+    response['sentiment'] = event['sentiment']
+        
+    return response
